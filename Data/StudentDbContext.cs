f00ae5fe2bbdee82d8d20b549a52c8c9b7c37bff
amd_404 <aniketmdeshmane@gmail.com>
2023-01-30 09:45:28 +0530

    Updated

---
+++ Data/StudentDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentManagement.Models.Domain;
+using System.Collections.Generic;
+
+namespace StudentManagement.Data
+{
+    public class StudentDbContext : DbContext
+    {
+        public StudentDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        //Table name
+        public DbSet<StudentModel> Students { get; set; }
+    }
+}
